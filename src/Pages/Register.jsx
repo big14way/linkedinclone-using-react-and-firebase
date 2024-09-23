a7f00c45a29de5a630d4e54c989e0a2d86d6a7e3
GODSWILL <idolorwilly@icloud.com>
2024-09-23 23:03:44 +0100

    changes

---
+++ src/Pages/Register.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import RegisterComponent from "../components/RegisterComponent";
+
+export default function Register() {
+  return <RegisterComponent />;
+}
+
+Routes 
+Index.jsx
+
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import HomeLayout from "../layouts/HomeLayout";
+import ProfileLayout from "../layouts/ProfileLayout";
+import ConnectionLayout from "../layouts/ConnectionLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/home",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/profile",
+    element: <ProfileLayout />,
+  },
+  {
+    path: "/connections",
+    element: <ConnectionLayout />,
+  },
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import {router } from './Routes';
-import { app } from "firebaseConfig";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes";
+import { app, auth } from "./firebaseConfig";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-quill/dist/quill.snow.css";
+import "./index.scss";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </React.StrictMode>
+);
